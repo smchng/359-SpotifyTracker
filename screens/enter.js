92c83d49b34297c1efbc296419d8e1f0f6c0fab3
Katrina Tam <kta85@sfu.ca>
2024-10-30 22:36:44 -0700

    added hello there! text on the enter page

--- screens/enter.js
+++ screens/enter.js
@@ -5,9 +5,27 @@ import { Buttons } from "../components/buttons";
 // Login with existing account
 export default function Enter({ navigation }) {
   return (
-    <View>
+    <View style={styles.container}>
+      <Text style={styles.greetingText}>Hello there!</Text>
       <Buttons text="Login" page="Login" navigation={navigation} />
       <Buttons text="Sign Up" page="SignUp" navigation={navigation} />
     </View>
   );
 }
+
+//home screen
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#F2F3F5', // Set background color
+    justifyContent: 'flex-start', // Align items to the top
+    alignItems: 'flex-start', // Align items to the left
+  },
+  greetingText: {
+    position: 'absolute', // Position the text absolutely
+    left: 50, // x position
+    top: 50, // y position
+    fontSize: 20, // Adjust font size as needed
+    color: '#000', // Text color (black)
+  },
+});
